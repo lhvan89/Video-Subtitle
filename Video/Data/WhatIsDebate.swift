823c35d824251484dd34b629f62747fa380a026a
Vấn Lê <vanlh@itpsolution.net>
2020-01-12 00:35:05 +0700

    update structure

---
+++ Video/Data/WhatIsDebate.swift
@@ -0,0 +1,34 @@
+//
+//  WhatIsDebate.swift
+//  Video
+//
+//  Created by mac on 9/8/19.
+//  Copyright © 2019 lhvan89. All rights reserved.
+//
+
+import Foundation
+
+var WhatIsDebate = Lesstion("What is debate?", "WhatIsDebate", [
+    Sub(6.35, 11.755, "From VOA learning English, this is the Education Report.", "Từ chương trình học tiếng Anh qua VOA, đây là Bản tin Giáo dục."),
+    Sub(11.755, 20.041, "As an English learner, you want to be able to understand and to discuss a subject.", "Là một người học tiếng Anh, bạn muốn có thể hiểu và thảo luận về một chủ đề."),
+    Sub(20.04, 27.998, "You also want to be able to express your opinions, and to support them.", "Bạn cũng muốn có thể bày tỏ quan điểm của mình, và bảo vệ những quan điểm đó."),
+    Sub(27.998, 33.641, "You can learn these skills by learning how to debate.", "Bạn có thể học được những kỹ năng này bằng việc học cách tranh luận."),
+    Sub(33.641, 41.48, "Americans use the word debate to talk about discussions among people.", "Người Mỹ sử dụng từ tranh luận để nói về các cuộc thảo luận giữa mọi người."),
+    Sub(41.48, 47.799, "Another use of the word debate describes a more formal discussion.", "Cách sử dụng khác của từ tranh luận là mô tả một cuộc thảo luận trang trọng hơn."),
+    Sub(47.799, 60.792, "Debaters work on a team to convince a judge that their opinion about a topic has more value than the opposite side's opinion.", "Những người tham gia tranh luận làm việc theo nhóm để thuyết phục một người phán xét rằng quan điểm của họ về một chủ đề nào đó có giá trị hơn quan điểm của đối phương."),
+    Sub(60.792, 71.179, "The National Speech and Debate Association says that students of debate research both sides of a topic.", "Hiệp hội Ngôn luận Quốc gia nói rằng sinh viên ngành ngôn luận nghiên cứu cả hai mặt của một vấn đề."),
+    Sub(71.179, 79.811, "They learn to think critically about every argument that could be made on each side.", "Họ học cách tư duy phản biện về mọi lý luận có thể được mỗi bên đưa ra."),
+    Sub(79.811, 86.095, "Debate comes from a tradition started in Ancient Greece.", "Tranh luận xuất phát từ một truyền thống bắt nguồn ở Hy Lạp cổ đại."),
+    Sub(86.095, 102.733, "They believed people needed to have certain skills in a democracy, including speaking in public in support of an opinion, andbeing able to explain that opinion.", "Họ tin rằng con người cần phải có các kỹ năng nhất định trong một nền dân chủ, trong đó có kỹ năng nói trước đám đông để ủng hộ cho một quan điểm, và có thể giải thích quan điểm đó."),
+    Sub(102.733, 115.985, "A debate contest traditionally has this structure: Teams agree to debate a statement, such as Nuclear power should bebanned.", "Một cuộc thi tranh luận truyền thống có cấu trúc như thế này: Các đội đồng ý tranh luận một nhận định chẳng hạn như Hạt nhân nguyên tử nên bị cấm."),
+    Sub(115.985, 119.534, "One team supports the resolution.", "Một đội ủng hộ nhận định trên."),
+    Sub(119.534, 123.456, "The other one is against the resolution.", "Đội còn lại phản đối nhận định đó."),
+    Sub(123.455, 130.656, "Each team gives an argument for or against the resolution.", "Mỗi đội đưa ra một ý kiến tranh luận ủng hộ hoặc phản đối nhận định này."),
+    Sub(130.656, 137.935, "Then each team asks questions about the other team's argument.", "Sau đó mỗi đội đặt câu hỏi về lập luận của đội khác."),
+    Sub(137.935, 147.52, "After answering the questions, each team makes more statements to support its arguments.", "Sau khi trả lời các câu hỏi, mỗi đội đưa ra nhiều nhận định hơn để hỗ trợ cho lập luận của mình."),
+    Sub(147.52, 155.894, "Finally, they summarize the arguments, they talk about them again, in less time.", "Cuối cùng họ tóm tắt các lập luận, họ nói lại một lần nữa, trong thời gian ngắn hơn."),
+    Sub(155.894, 165.348, "In a classic debate, teams may take 12 turns, in which one member of the team speaks.", "Trong một cuộc tranh luận truyền thống, các đội thay nhau nói 12 lượt, trong đó một thành viên trong nhóm nói một lượt."),
+    Sub(165.348, 171.341, "Join us next week for more information on debates.", "Hãy tham gia với chúng tôi trong tuần tới để biết thêm nhiều thông tin hơn về tranh luận."),
+    Sub(171.341, 175.524, "For VOA learning English, I'm Alex Villarreal.", "Từ chương trình học tiếng Anh qua VOA, tôi là Alex Villarreal.")
+    ]
+)
